0f1941384f64889281d218b37bbafe76b48d5c4c
KaileyLiou <kaileyliou09@gmail.com>
2024-12-01 22:27:05 -0500

    added files

---
+++ src/test/java/robot/drive/Drive.java
@@ -0,0 +1,15 @@
+package robot.drive;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import robot.Ports;
+
+public class Drive extends SubsystemBase {  
+    private final CANSparkMax leftLeader = new CANSparkMax(Ports.Drive.LEFT_LEADER, MotorType.kBrushless);
+    private final CANSparkMax leftFollower = new CANSparkMax(Ports.Drive.LEFT_FOLLOWER, MotorType.kBrushless);
+    private final CANSparkMax rightLeader = new CANSparkMax(Ports.Drive.RIGHT_LEADER, MotorType.kBrushless);
+    private final CANSparkMax rightFollower = new CANSparkMax(Ports.Drive.RIGHT_FOLLOWER, MotorType.kBrushless);
+
+  }
